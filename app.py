c196ab464286110201b34468d0384c9dfa650da0
Niko Typpö <niko.typpo@protonmail.com>
2025-05-13 17:26:58 +0300

    Add  registration

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask import render_template, request
+from werkzeug.security import generate_password_hash
+import db
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/create_account", methods=["POST"])
+def create_account():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        return "ERROR: Passwords are not the same!"
+    password_hash = generate_password_hash(password1)
+
+    try:
+        sql = "INSERT INTO users (username, password_hash) VALUES (?, ?)"
+        db.execute(sql, [username, password_hash])
+    except sqlite3.IntegrityError:
+        return "ERROR: Username taken"
+
+    return "Account created"
